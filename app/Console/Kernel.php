5abe0e64aba6be068a1031b068a647d2811b179b
ruslana-novikova22 <novikova.ruslana@student.uzhnu.edu.ua>
2024-03-23 23:00:24 +0200

    Shedules

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,11 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use App\Models\Advertisement;
+use App\Models\WorkerTask;
+use App\Models\Work;
+use App\Models\Washing;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,6 +18,23 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        
+        $schedule->call(function(){
+            //Deleting Advertisements what created more then 30 days ago
+            $thresholdDate = now()->subDays(30);
+            Advertisement::where('created_at', '<', $thresholdDate)->delete();
+
+            //Deleting WorkerTasks what created more then 30 days ago
+            WorkerTask::where('created_at', '<', $thresholdDate)->delete();
+
+            //Deleting Work what created more then 60 days ago
+            $thresholdDate = now()->subDays(60);
+            Work::where('created_at', '<', $thresholdDate)->delete();
+
+            //Deleting Washing what day washing more than 7 days ago
+            $thresholdDate = now()->subDays(7);
+            Washing::where('day', '<', $thresholdDate)->delete();
+        })->daily();
     }
 
     /**
